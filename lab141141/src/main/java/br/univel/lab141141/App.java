01825e4086d03b8d4c9ffa09092b1ff3a82b0d58
voliveira7 <viniciusdeoliveira93@gmail.com>
2016-08-11 21:57:45 -0300

    Importação do projeto #1

---
+++ lab141141/src/main/java/br/univel/lab141141/App.java
@@ -0,0 +1,13 @@
+package br.univel.lab141141;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
